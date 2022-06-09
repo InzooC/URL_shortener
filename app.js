2bd1ff42cc754ff81c7fd9a83b418c4d9d513e9e
InzooC <bluefreaks@gmail.com>
2022-06-09 21:58:03 +0800

    modify generateSerial function

--- app.js
+++ app.js
@@ -57,11 +57,12 @@ app.post('/', (req, res) => {
 // 製作新序號的function
 function generateSerial() {
   const number = '0123456789'
-  const alphabet = 'abcdefghijklmnopqrstuvwxyz'
-  const element = number.concat(alphabet).split('')
+  const lowerCaseAlphabet = 'abcdefghijklmnopqrstuvwxyz'
+  const upperCaseAlphabet = lowerCaseAlphabet.toUpperCase()
+  const element = number.concat(lowerCaseAlphabet).concat(upperCaseAlphabet).split('')
   let serial = ''
   for (let i = 0; i < 5; i++) {
-    serial += element[Math.floor(Math.random() * 36)]
+    serial += element[Math.floor(Math.random() * 62)]
   }
   return serial
 }
